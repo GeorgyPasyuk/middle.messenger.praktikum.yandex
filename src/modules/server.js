a91191400e7e0725534e88498643d2250894659b
Georgy Pasyuk <pasyukgeorgy@gmail.com>
2023-04-09 23:54:41 +0300

    port fix

--- src/modules/server.js
+++ src/modules/server.js
@@ -6,9 +6,10 @@ const PORT = 3000;
 
 
 app.use('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
 });
+
